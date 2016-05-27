a2a4ab36f538bcd8efd228dc9aada86229d0c3eb
Grace <grace.h.gamble@gmail.com>
2016-05-27 02:05:19 +0000

    Final Update to JS5 Edit in Place

--- js5.js
+++ js5.js
@@ -8,23 +8,23 @@ $(document).ready(function() { //Added Jquery to JavaScript file
         //Un-hide the <input>/.input-box
         var id = p.attr("data-id"); //This assigns the variable id to the clicked <p> of the three available to click
         console.log(id); //This prints to the console the <p> that was clicked on
-        var input = $("#" + id); //
-        input.removeClass("hidden");
-        input.select();
+        var input = $("#" + id); //This assigns a variable called input that is a function of the variable id combined with an id assignment
+        input.removeClass("hidden"); //This removes the class of hidden from the input tag so that it is now visible
+        input.select(); //This selects the input that will be edited with new text
     });
-    $(".input-box").blur(function(event){
-        console.log("blurred");
+    $(".input-box").blur(function(event){ //Creating a function called blur taking the class of input-box
+        console.log("blurred"); //This prints to the console the line that has been focused on by clicking in the input box to acknowledge what line has been focused on
         //Hide the <p>/.change-this
-        var input = $(event.currentTarget);
+        var input = $(event.currentTarget); //This creates a variable called input which is the function of the event of a user clicking on one of the input boxes
         var changedText = input.val(); //Takes text that was input and saves as variable
-        console.log(changedText);
-        input.addClass("hidden");
+        console.log(changedText); //This prints to the console the the changed text that was in the input box that the user selected
+        input.addClass("hidden"); //This hides the input box that was edited
         //Un-hide the <input>/.input-box
-        var id = input.attr("id");
-        console.log(id);
-        var p = $("[data-id='" + id + "']");
-        p.text(changedText);
-        p.removeClass("hidden");
+        var id = input.attr("id"); //This assigns a new variable called id that equals the attribute id of the associated input box
+        console.log(id); //This prints to the console the new variable that equals the attribute assigned to the input box that the user selected
+        var p = $("[data-id='" + id + "']"); //This assigns a new variable called p which equals the newly updated p line
+        p.text(changedText); //This updates the p with the new text that was entered in the input
+        p.removeClass("hidden"); //This un-hides the p so that the new text is now visible
         
     });
 });
